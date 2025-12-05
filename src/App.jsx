fa778adfe7d670d3a04a34cde50a62ca661844cd
psudocode-monk <avisheksadhukhan7@gmail.com>
2025-12-05 17:38:22 +0530

    Fix UI in Mobile

--- src/App.jsx
+++ src/App.jsx
@@ -109,7 +109,14 @@ function App() {
           <button
             type="submit"
             disabled={!streakDay || !topicName || !projectUrl}
-            className="mt-2 w-full inline-flex items-center justify-center rounded-xl bg-emerald-500 px-4 py-2.5 text-sm sm:text-base font-semibold text-slate-950 shadow-lg shadow-emerald-500/30 hover:bg-emerald-400 active:bg-emerald-500 transition cursor-pointer"
+            className={`mt-2 w-full inline-flex items-center justify-center rounded-xl px-4 py-2.5 
+    text-sm sm:text-base font-semibold text-slate-950 shadow-lg transition
+    ${
+      !streakDay || !topicName || !projectUrl
+        ? "bg-emerald-500/40 cursor-not-allowed opacity-50 shadow-none"
+        : "bg-emerald-500 shadow-emerald-500/30 hover:bg-emerald-400 active:bg-emerald-500 cursor-pointer"
+    }
+  `}
           >
             {loading ? <Loader className="animate-spin" /> : "Save"}
           </button>
